49d2bdf3a6a56317559bdf646154d880c1d21538
sirlencio <carlos.l.p.251001@gmail.com>
2022-11-28 09:54:32 +0100

    v.0

--- Practica Hilos/src/ej4/Ejercicio4.java
+++ Practica Hilos/src/ej4/Ejercicio4.java
@@ -2,13 +2,9 @@ package ej4;
 
 public class Ejercicio4 {
     public static void main(String[] args) {
-        Thread hilo = new Thread();
-        SistemaDeRed intento = new SistemaDeRed();
-        hilo.run();
     }
 }
-class SistemaDeRed{
-
+class SistemaDeRed {
     public void claveOk(String clave){
 
     }
